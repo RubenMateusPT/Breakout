2fbacd29099f0c67312af34a7b33a0d886e9716e
Ruben Mateus <rubenmateus.info@gmail.com>
2024-10-22 23:06:02 +0100

    Fixed My own error with constexpre

--- Breakout/include/Breakout/Managers/GameManager.h
+++ Breakout/include/Breakout/Managers/GameManager.h
@@ -44,8 +44,8 @@ private:
     PowerupManager* _powerupManager;
     UI* _ui;
     
-    constexpr float POWERUP_FREQUENCY = 7.5f;    // time between minimum powerup spawn
-    constexpr int POWERUP_MAX_RANDOMIZE_FREQUENCY = 700; // a random max time for powerup to spawn (based on minumum time
+    static constexpr float POWERUP_FREQUENCY = 7.5f;    // time between minimum powerup spawn
+    static constexpr int POWERUP_MAX_RANDOMIZE_FREQUENCY = 700; // a random max time for powerup to spawn (based on minumum time
 
     //this should be done on UI
     void setMasterText(std::string message);
